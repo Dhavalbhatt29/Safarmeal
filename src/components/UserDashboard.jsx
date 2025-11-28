adc00867bdbf3fe241491f0ccfc6c07ae3a29e59
safartechlab <safartechlab@gmail.com>
2025-11-28 11:25:44 +0530

    c

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import Nav from './nav.jsx'
+import Nav from './Nav.jsx'
 import { categories } from '../category'
 import CategoryCard from './CategoryCard'
 import { FaCircleChevronLeft } from "react-icons/fa6";
